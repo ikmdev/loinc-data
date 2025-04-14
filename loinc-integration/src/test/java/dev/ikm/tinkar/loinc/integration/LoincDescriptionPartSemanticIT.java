8be5483e639fee3d4d3da49cffade298acea84cf
Joshua Dettinger <joshuadettinger@gmail.com>
2025-04-14 08:45:21 -0500

    Feature/ikdt 869 rename definition to description (#30)
    * Update version to start feature: IKDT-869-rename-Definition-to-Description
    
    * IKDT-869 Change IT file names from Definition to Description
    
    * IKDT-869 fix versions

--- loinc-integration/src/test/java/dev/ikm/tinkar/loinc/integration/LoincDescriptionPartSemanticIT.java
+++ loinc-integration/src/test/java/dev/ikm/tinkar/loinc/integration/LoincDescriptionPartSemanticIT.java
@@ -1,10 +1,6 @@
 package dev.ikm.tinkar.loinc.integration;
 
-import dev.ikm.maven.LoincUtility;
-import dev.ikm.tinkar.common.id.PublicId;
 import dev.ikm.tinkar.common.id.PublicIds;
-import dev.ikm.tinkar.common.util.uuid.UuidT5Generator;
-import dev.ikm.tinkar.common.util.uuid.UuidUtil;
 import dev.ikm.tinkar.component.Component;
 import dev.ikm.tinkar.coordinate.Calculators;
 import dev.ikm.tinkar.coordinate.Coordinates;
@@ -13,13 +9,9 @@ import dev.ikm.tinkar.coordinate.stamp.StateSet;
 import dev.ikm.tinkar.coordinate.stamp.calculator.Latest;
 import dev.ikm.tinkar.coordinate.stamp.calculator.StampCalculator;
 import dev.ikm.tinkar.coordinate.stamp.calculator.StampCalculatorWithCache;
-import dev.ikm.tinkar.entity.ConceptRecord;
-import dev.ikm.tinkar.entity.ConceptVersionRecord;
 import dev.ikm.tinkar.entity.EntityService;
 import dev.ikm.tinkar.entity.PatternEntityVersion;
 import dev.ikm.tinkar.entity.SemanticEntityVersion;
-import dev.ikm.tinkar.entity.SemanticRecord;
-import dev.ikm.tinkar.entity.SemanticVersionRecord;
 import dev.ikm.tinkar.terms.EntityProxy;
 import dev.ikm.tinkar.terms.EntityProxy.Concept;
 import dev.ikm.tinkar.terms.TinkarTerm;
@@ -27,139 +19,117 @@ import dev.ikm.tinkar.terms.TinkarTerm;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 
-import static dev.ikm.tinkar.terms.TinkarTerm.DEFINITION_DESCRIPTION_TYPE;
 import static dev.ikm.tinkar.terms.TinkarTerm.DESCRIPTION_NOT_CASE_SENSITIVE;
 import static dev.ikm.tinkar.terms.TinkarTerm.FULLY_QUALIFIED_NAME_DESCRIPTION_TYPE;
 import static dev.ikm.tinkar.terms.TinkarTerm.REGULAR_NAME_DESCRIPTION_TYPE;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class LoincDefinitionSemanticIT extends LoincAbstractIntegrationTest {
+public class LoincDescriptionPartSemanticIT extends LoincAbstractIntegrationTest {
 	
 	/**
-	 * Test LoincDefinition Loinc.csv Semantics.
+	 * Test LoincDescriptionPart Part.csv Semantics.
 	 *
 	 * @result Reads content from file and validates Concept of Semantics by calling
 	 *         private method assertConcept().
 	 */
 	@Test
-	public void testLoincDefinitionSemantics() throws IOException {
-		String sourceFilePath = "../loinc-origin/target/origin-sources";
-		String errorFile = "target/failsafe-reports/LoincCsv_definitions_not_found.txt";
+	public void testLoincDescriptionPartSemantics() throws IOException {
+        String sourceFilePath = "../loinc-origin/target/origin-sources";
+        String errorFile = "target/failsafe-reports/PartCsv_descriptions_not_found.txt";
 
-		String absolutePath = findFilePath(sourceFilePath, "Loinc.csv");
-		int notFound = processLoincFile(absolutePath, errorFile);
-		
-		assertEquals(0, notFound,
-				"Unable to find " + notFound + " Loinc.csv 'Definition' semantics. Details written to " + errorFile);
+        String absolutePath = findFilePath(sourceFilePath, "Part.csv");
+        int notFound = processPartFile(absolutePath, errorFile);
+
+        assertEquals(0, notFound, "Unable to find " + notFound + " Part.csv semantics. Details written to " + errorFile);
 	}
-
-	@Override
-	protected boolean assertLine(String[] columns) {
-		Map<UUID, ConceptMapValue> termConceptMap = new HashMap<>();
-		
-		UUID id = uuid(columns[0]);
-
-		AtomicInteger innerCount = new AtomicInteger(0);
-		
-		String longCommonName = removeQuotes(columns[25]);
-		String consumerName = removeQuotes(columns[12]);
-		String shortName = removeQuotes(columns[20]);
-		String relatedNames2 = removeQuotes(columns[19]);
-		String displayName = removeQuotes(columns[39]);
-		String definitionDescription = removeQuotes(columns[10]);
-
+	
+    @Override
+    protected boolean assertLine(String[] columns) {
+        UUID id = uuid(columns[0]);
+        
+		Map<UUID, ConceptMapValue> termConceptMap = new HashMap<>(); // is it PartDisplayName or PartName
+        
 		EntityProxy.Concept concept = EntityProxy.Concept.make(PublicIds.of(id));
 
-		// Create description semantics for non-empty fields
-		if (!longCommonName.isEmpty()) {
-			termConceptMap.put(getConceptMapKey(concept, longCommonName, "FQN"), getConceptMapValue(FULLY_QUALIFIED_NAME_DESCRIPTION_TYPE, longCommonName));
-		}
-
-		if (!consumerName.isEmpty()) {
-			termConceptMap.put(getConceptMapKey(concept, consumerName, "Regular"), getConceptMapValue(REGULAR_NAME_DESCRIPTION_TYPE, consumerName));
-		}
-
-		if (!shortName.isEmpty()) {
-			termConceptMap.put(getConceptMapKey(concept, shortName, "Regular"), getConceptMapValue(REGULAR_NAME_DESCRIPTION_TYPE, shortName));
-		}
-
-		if (!relatedNames2.isEmpty()) {
-			termConceptMap.put(getConceptMapKey(concept, relatedNames2, "Regular"), getConceptMapValue(REGULAR_NAME_DESCRIPTION_TYPE, relatedNames2));
-		}
-
-		if (!displayName.isEmpty()) {
-			termConceptMap.put(getConceptMapKey(concept, displayName, "Regular"), getConceptMapValue(REGULAR_NAME_DESCRIPTION_TYPE, displayName));
-		}
-
-		if (!definitionDescription.isEmpty()) {
-			termConceptMap.put(getConceptMapKey(concept, definitionDescription, "Definition"), getConceptMapValue(DEFINITION_DESCRIPTION_TYPE, definitionDescription));
-		}
-
+		String partName = removeQuotes(columns[2]);
+		String partDisplayName = removeQuotes(columns[3]);
+			
 		final StateSet active;
-		if (columns[11].equals("ACTIVE") || columns[11].equals("TRIAL") || columns[11].equals("DISCOURAGED")) {
+		if (columns[4].equals("ACTIVE") || columns[4].equals("TRIAL") || columns[4].equals("DISCOURAGED")) {
 			active = StateSet.ACTIVE;
 		} else {
 			active = StateSet.INACTIVE;
 		}
 
 		AtomicBoolean matched = new AtomicBoolean(true);
+		AtomicInteger innerCount = new AtomicInteger(0);
+
+		// Create description semantics for non-empty fields
+		// FULLY_QUALIFIED_NAME_DESCRIPTION_TYPE; // decriptionType for PartDisplayName
+		// REGULAR_NAME_DESCRIPTION_TYPE;         // decriptionType for PartName
+		if (!partName.isEmpty()) {
+			termConceptMap.put(getConceptMapKey(concept, partName, "Regular"), getConceptMapValue(REGULAR_NAME_DESCRIPTION_TYPE, partName));
+		}	
+		
+		if (!partDisplayName.isEmpty()) {
+			termConceptMap.put(getConceptMapKey(concept, partDisplayName, "FQN"), getConceptMapValue(FULLY_QUALIFIED_NAME_DESCRIPTION_TYPE, partDisplayName));
+		}
 		
 		StampCalculator stampCalc = StampCalculatorWithCache
 				.getCalculator(StampCoordinateRecord.make(active, Coordinates.Position.LatestOnMaster()));
 		
 		PatternEntityVersion latestDescriptionPattern = (PatternEntityVersion) Calculators.Stamp.DevelopmentLatest()
 				.latest(TinkarTerm.DESCRIPTION_PATTERN).get();
-		
+        
 		EntityService.get().forEachSemanticForComponentOfPattern(concept.nid(), TinkarTerm.DESCRIPTION_PATTERN.nid(), semanticEntity -> {
 			innerCount.incrementAndGet();
 			
 			Latest<SemanticEntityVersion> latest = stampCalc.latest(semanticEntity);
 			UUID semanticEntityUUID = semanticEntity.asUuidArray()[0];
-			
+	
 			ConceptMapValue cmv = termConceptMap.get(semanticEntityUUID);
-						
+			
 			if(cmv != null) {
 				if (latest.isPresent()) {
+					
 					Component descriptionType = latestDescriptionPattern.getFieldWithMeaning(TinkarTerm.DESCRIPTION_TYPE,
 							latest.get());
-					
+												
 					Component caseSensitivity = latestDescriptionPattern
 							.getFieldWithMeaning(TinkarTerm.DESCRIPTION_CASE_SIGNIFICANCE, latest.get());
-					
+
 					String text = latestDescriptionPattern.getFieldWithMeaning(TinkarTerm.TEXT_FOR_DESCRIPTION, latest.get());
-											
-					if (!descriptionType.equals(cmv.conceptDescType) || !caseSensitivity.equals(DESCRIPTION_NOT_CASE_SENSITIVE) 
-								|| !text.equals(cmv.term)) {
-		
+
+					if (!descriptionType.equals(cmv.conceptDescType) || !caseSensitivity.equals(DESCRIPTION_NOT_CASE_SENSITIVE)
+							|| !text.equals(cmv.term)) {
+
 						matched.set(false);
-					}	
+					}
+				} else {
+					matched.set(false);
 				}
-			} 
-		});
-		
+			} else {
+				matched.set(false);
+			}
+		});	
+
 		if(innerCount.get() == termConceptMap.size()) {
-			innerCount.set(0);
 			return matched.get();
 		} 
 		
-		innerCount.set(0);
 		return false;
-	}
-	
+    }
 	private ConceptMapValue getConceptMapValue(Concept conceptDescType, String term) {
 		return new ConceptMapValue(conceptDescType, term);
 	}
-	
+
 	private UUID getConceptMapKey(Concept concept, String term, String typeStr) {
 		return uuid(concept.publicId().asUuidArray()[0] + term + typeStr + "DESC");
 	}
-
 }
